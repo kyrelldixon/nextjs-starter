f40875cef825d7a1584aa452c21b1cdaf33833c9
Kyrell Dixon <kyrell.brown@gmail.com>
2023-12-18 12:15:17 -0600

    refactor: change home to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { Counter } from '~/components/counter'
 
-export default function Home() {
+export default function HomePage() {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
       <h1 className="text-6xl font-bold">Next.js Starter</h1>
